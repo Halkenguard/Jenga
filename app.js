5cfb60bd0ead5afa9cd0755937c34fd3b8d0644d
Kevin Nunley <kevinmn1996@gmail.com>
2018-08-31 11:03:14 -0400

    Added button functuality
    Added functionality to the 'Generate Rules' and 'View/Edit Rules' buttons. These now display the gen-list and ruleset divs when pressed. Return functionality not yet implemented.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const genButton =  document.getElementsByClassName("gen-button");
 
-genButton.onclick = () => console.error("GenButton clicked");
+// Variable declarations
+const genButton = document.querySelector('.gen-button');
+const returnButton = document.querySelector('.return');
+const editButton = document.querySelector('.edit-button')
+const genListDiv = document.querySelector('#gen-list');
+const rulesetDiv = document.querySelector('#ruleset');
+const mainContent = document.querySelector('#main-content');
+
+// Event Listeners
+genButton.addEventListener('click', () => {displayDiv(genListDiv)});
+editButton.addEventListener('click', () => {displayDiv(rulesetDiv)});
+
+// Functions
+function displayDiv(div){
+    mainContent.style.display = 'none';
+    div.style.display = 'block';
+}
